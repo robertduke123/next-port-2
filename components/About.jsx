4afb3a0264829ea6897c027568d04829ff29aca9
Robert Duke <robduke123@gmail.com>
2025-03-30 23:52:58 +1300

    added about transitions

--- components/About.jsx
+++ components/About.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faLocationDot,
@@ -6,33 +7,70 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
 import React from "react";
+import { useInView } from "react-intersection-observer";
 
 const About = () => {
+	const { ref: ref1, inView: isIntersecting1 } = useInView();
+	const { ref: ref2, inView: isIntersecting2 } = useInView();
+	const { ref: ref3, inView: isIntersecting3 } = useInView();
+	const { ref: ref4, inView: isIntersecting4 } = useInView();
+	const { ref: ref5, inView: isIntersecting5 } = useInView();
+	const { ref: ref6, inView: isIntersecting6 } = useInView();
+	const { ref: ref7, inView: isIntersecting7 } = useInView();
+	const { ref: ref8, inView: isIntersecting8 } = useInView();
+	const { ref: ref9, inView: isIntersecting9 } = useInView();
+	const { ref: ref10, inView: isIntersecting10 } = useInView();
+	const { ref: ref11, inView: isIntersecting11 } = useInView();
+	const { ref: ref12, inView: isIntersecting12 } = useInView();
+	const { ref: ref13, inView: isIntersecting13 } = useInView();
+	const { ref: ref14, inView: isIntersecting14 } = useInView();
+	const { ref: ref15, inView: isIntersecting15 } = useInView();
+
 	return (
 		<div className="w-full flex justify-center">
 			<div className="w-2/3 flex justify-center">
 				<div className="w-1/2 pl-25 pr-5 py-10 flex flex-col justify-around items-start">
 					<h2 className="text-xl text-gray-500">About Me</h2>
-					<div className="my-5 text-6xl">
+					<div
+						ref={ref1}
+						className={`my-5 text-6xl ${
+							!isIntersecting1 ? "-translate-x-20 opacity-0" : "opacity-100"
+						}`}
+						style={{ transition: "0.4s" }}>
 						<h1>Hi, I'm</h1>
 						<h1 className="text-blue-300">Robert Duke</h1>
 					</div>
 					<div className=" h-25 flex flex-col justify-around">
-						<span className="text-md text-gray-500 flex">
+						<span
+							ref={ref2}
+							className={`text-md text-gray-500 flex ${
+								!isIntersecting2 ? "-translate-x-20 opacity-0" : "opacity-100"
+							}`}
+							style={{ transition: "0.6s" }}>
 							<FontAwesomeIcon
 								icon={faLocationDot}
 								className="w-4 mr-5 text-blue-400"
 							/>
 							Based in Auckland, New Zealand
 						</span>
-						<span className="text-md text-gray-500 flex">
+						<span
+							ref={ref3}
+							className={`text-md text-gray-500 flex ${
+								!isIntersecting3 ? "-translate-x-20 opacity-0" : "opacity-100"
+							}`}
+							style={{ transition: "0.6s" }}>
 							<FontAwesomeIcon
 								icon={faSquarePlus}
 								className="w-4 mr-5 text-blue-300"
 							/>
 							28 Years Old
 						</span>
-						<span className="text-md text-gray-500 flex">
+						<span
+							ref={ref4}
+							className={`text-md text-gray-500 flex ${
+								!isIntersecting4 ? "-translate-x-20 opacity-0" : "opacity-100"
+							}`}
+							style={{ transition: "0.6s" }}>
 							<FontAwesomeIcon
 								icon={faCode}
 								className="w-4 mr-5 text-blue-300"
@@ -41,14 +79,24 @@ const About = () => {
 						</span>
 					</div>
 
-					<p className="text-xl my-5">
+					<p
+						ref={ref5}
+						className={`text-xl my-5 ${
+							!isIntersecting5 ? "-translate-x-20 opacity-0" : "opacity-100"
+						}`}
+						style={{ transition: "0.6s" }}>
 						I am a passionate front-end developer who thrives on creating
 						impactful, accessible, and engaging web applications, whether
 						working as part of a team or individually.
 					</p>
-					<div className="w-full pr-5 flex justify-around">
+					<div
+						ref={ref6}
+						className={`w-full pr-5 flex justify-around ${
+							!isIntersecting6 ? "translate-y-20 opacity-0" : "opacity-100"
+						}`}
+						style={{ transition: "0.4s" }}>
 						<button
-							className="cursor-pointer w-38 h-13 rounded-lg py-2 text-center bg-blue-400 hover:-translate-y-0.5"
+							className={`cursor-pointer w-38 h-13 rounded-lg py-2 text-center bg-blue-400 hover:-translate-y-0.5`}
 							style={{ transition: "0.2s" }}>
 							Get In Touch
 						</button>
@@ -60,37 +108,84 @@ const About = () => {
 					</div>
 				</div>
 
-				<div className="w-1/2 pl-10 pr-5 py-10 flex flex-col items-start">
+				<div
+					ref={ref7}
+					className={`w-1/2 pl-10 pr-5 py-10 flex flex-col items-start  ${
+						!isIntersecting7 ? "translate-x-20 opacity-0" : "opacity-100"
+					}`}
+					style={{ transition: "0.4s" }}>
 					<h2 className="pl-25 pr-5 text-2xl my-8">Skills</h2>
 
 					<ul className="w-full flex flex-wrap">
-						<li className="w-24 h-10 m-2 py-3 text-xs text-center text-gray-400 rounded-3xl bg-blue-900/30">
+						<li
+							ref={ref8}
+							className={`w-24 h-10 m-2 py-3 text-xs text-center text-gray-400 rounded-3xl bg-blue-900/30 ${
+								!isIntersecting8 ? "translate-y-20 opacity-0" : "opacity-100"
+							}`}
+							style={{ transition: "0.6s" }}>
 							Next.js
 						</li>
-						<li className="w-24 h-10 m-2 py-3 text-xs text-center text-gray-400 rounded-3xl bg-blue-900/30">
+						<li
+							ref={ref9}
+							className={`w-24 h-10 m-2 py-3 text-xs text-center text-gray-400 rounded-3xl bg-blue-900/30 ${
+								!isIntersecting9 ? "translate-y-20 opacity-0" : "opacity-100"
+							}`}
+							style={{ transition: "0.8s" }}>
 							React
 						</li>
-						<li className="w-24 h-10 m-2 py-3 text-xs text-center text-gray-400 rounded-3xl bg-blue-900/30">
+						<li
+							ref={ref10}
+							className={`w-24 h-10 m-2 py-3 text-xs text-center text-gray-400 rounded-3xl bg-blue-900/30 ${
+								!isIntersecting10 ? "translate-y-20 opacity-0" : "opacity-100"
+							}`}
+							style={{ transition: "1s" }}>
 							TailwindCSS
 						</li>
-						<li className="w-24 h-10 m-2 py-3 text-xs text-center text-gray-400 rounded-3xl bg-blue-900/30">
+						<li
+							ref={ref11}
+							className={`w-24 h-10 m-2 py-3 text-xs text-center text-gray-400 rounded-3xl bg-blue-900/30 ${
+								!isIntersecting11 ? "translate-y-20 opacity-0" : "opacity-100"
+							}`}
+							style={{ transition: "1.2s" }}>
 							PostgresSQL
 						</li>
-						<li className="w-24 h-10 m-2 py-3 text-xs text-center text-gray-400 rounded-3xl bg-blue-900/30">
+						<li
+							ref={ref12}
+							className={`w-24 h-10 m-2 py-3 text-xs text-center text-gray-400 rounded-3xl bg-blue-900/30 ${
+								!isIntersecting12 ? "translate-y-20 opacity-0" : "opacity-100"
+							}`}
+							style={{ transition: "1.4s" }}>
 							express
 						</li>
 					</ul>
 
-					<h2 className="text-2xl my-8">Services</h2>
+					<h2
+						ref={ref13}
+						className={`text-2xl my-8 ${
+							!isIntersecting12 ? "translate-x-20 opacity-0" : "opacity-100"
+						}`}
+						style={{ transition: "0.4s" }}>
+						Services
+					</h2>
 					<ul>
-						<li>
+						<li
+							ref={ref14}
+							className={
+								!isIntersecting12 ? "translate-x-20 opacity-0" : "opacity-100"
+							}
+							style={{ transition: "0.4s" }}>
 							<FontAwesomeIcon
 								icon={faArrowRight}
 								className="w-4 mr-5 text-blue-400"
 							/>
 							Full Stack Development
 						</li>
-						<li>
+						<li
+							ref={ref15}
+							className={
+								!isIntersecting12 ? "translate-x-20 opacity-0" : "opacity-100"
+							}
+							style={{ transition: "0.4s" }}>
 							<FontAwesomeIcon
 								icon={faArrowRight}
 								className="w-4 mr-5 text-blue-400"
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { projects } from "@/data";
 import { useInView } from "react-intersection-observer";
 import ProjectCard from "./ui/ProjectCard";
@@ -15,15 +15,6 @@ const Projects = () => {
 			id="projects"
 			className="relative w-full flex flex-col items-center 
 			">
-			{/* <div className="absolute w-full grid grid-cols-22 gap-x-5 -top-5 overflow-hidden">
-				{Array(650)
-					.fill("")
-					.map((_, idx) => (
-						<div
-							key={"bg-shape " + idx}
-							className=" w-6 h-15 rotate-60 bg-gray-900/25"></div>
-					))}
-			</div> */}
 			<div className="h-[1px] w-5/6 bg-gray-400"></div>
 			<h1 className="relative z-5 w-full h-20 px-45 pt-7 text-center text-lg">
 				My Projects
