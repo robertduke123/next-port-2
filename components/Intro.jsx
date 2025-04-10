adf96bbaf4edd52d3e5b0bba8a16e88fbf972b57
Robert Duke <robduke123@gmail.com>
2025-04-10 18:15:41 +1200

    intro started

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 
 const Intro = () => {
 	const [text, setText] = useState("hello");
+	const [dis, setDis] = useState(true);
 	useEffect(() => {
 		setTimeout(() => {
 			setText("hola");
@@ -13,10 +14,17 @@ const Intro = () => {
 		setTimeout(() => {
 			setText("benjoure");
 		}, 3000);
+		setTimeout(() => {
+			setDis(false);
+		}, 4000);
 	}, []);
 
 	return (
-		<div className="fixed z-50 w-full h-screen text-5xl bg-black-100 flex justify-center items-center">
+		<div
+			className={`fixed z-50 w-full h-screen text-5xl bg-black-100 flex justify-center items-center ${
+				dis ? "" : "-mt-250 opacity-0"
+			}`}
+			style={{ transition: "0.3s, opacity 1s" }}>
 			{text.toUpperCase()}
 		</div>
 	);
