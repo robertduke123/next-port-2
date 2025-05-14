0eb23785accee5eb50f9d1b55290343a3c3778d2
Robert Duke <robduke123@gmail.com>
2025-05-14 17:05:44 +1200

    mobile changes added

--- components/ui/Scroll.jsx
+++ components/ui/Scroll.jsx
@@ -7,7 +7,7 @@ const Scroll = () => {
 
 	return (
 		<div
-			className="hidden sm:flex fixed z-20 w-20 top-35 right-5 h-80 justify-center items-center bg-black-100 "
+			className="hidden md:flex fixed z-20 w-20 top-35 right-5 h-80 justify-center items-center bg-black-100 "
 			style={{ boxShadow: "-5px 5px 5px 5px #01030f" }}>
 			<span className="hidden xl:block w-5 mr-5 text-center text-gray-400/50 text-sm">
 				{Math.floor((scroll / 3252.3) * 100)}
@@ -17,9 +17,6 @@ const Scroll = () => {
 			</span>
 			<span className="hidden md:block lg:hidden w-5 mr-5 text-center text-gray-400/50 text-sm">
 				{Math.floor((scroll / 3532.3) * 100)}
-			</span>
-			<span className="block md:hidden w-5 mr-5 text-center text-gray-400/50 text-sm">
-				{Math.floor((scroll / 3562.3) * 100)}
 			</span>
 
 			<div
